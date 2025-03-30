408d42a08d4ec8998bc1627c37430cb896e20512
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-03-31 00:08:14 +0300

    r files

--- R_Files/Customer Sales Analyis/Customer Sales Analysis.R
+++ R_Files/Customer Sales Analyis/Customer Sales Analysis.R
@@ -29,7 +29,30 @@ glimpse(sales_data)
 
 #Exploratory Data Analysis
 summary(sales_data$Sales)
+# Checking for outliers in sales
 ggplot(data = sales_data, mapping = aes(x= Category, y= Sales )) +
-  geom_boxplot(fill="steelblue", width=0.8) +
+  geom_boxplot(fill="steelblue", width=0.8, notch=TRUE, outlier.colour="red", outlier.shape=8) +
   labs(title = "Sales per Product Category", xlab ="Categories", ylab ="Sales")+ 
   theme_minimal()
+
+# Total Sales over time
+ggplot(data= sales_data, mapping = aes(x=`Order Date`, y = Sales)) +
+  geom_line(color="steelblue")+
+  labs(title = "Total Sales Over Time", x="Date",Y= "Sales")
+# Top selling categories
+ggplot(data = sales_data, mapping = aes(x= Category, y= Sales, fill = Category)) +
+  geom_bar(stat="identity", width=0.5) +
+  scale_fill_manual(values=c("red", "blue", "green"))+
+  labs(title="Top Selling Categories", x="Categories", Ylab= "Sales") + 
+  theme_minimal()
+# Sales by region
+ggplot(data = sales_data, mapping = aes(x= Region, y= Sales, fill=Region)) +
+  geom_bar(stat="identity", width=0.5, fill="steelblue") +
+  labs(title="Sales By Regions", x="Regions", Ylab= "Sales") +
+  coord_flip() +
+  theme_minimal()
+# Customer Purchase frequency
+ggplot(data= sales_data, mapping = aes( x = `Customer ID`, y = Sales)) +
+  geom_line(color="steelblue") +
+  labs(title ="Customer Purchase Frequency", xlab= "Customer ID", y= "Sales") +
+  theme_classic()
